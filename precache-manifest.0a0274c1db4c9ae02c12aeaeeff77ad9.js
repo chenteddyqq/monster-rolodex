4d03e5678de9683579db919662d1c093c1eeac3c
Ted chen <chenteddy@outlook.com>
2019-10-21 14:08:22 +0800

    Updates

---
+++ precache-manifest.0a0274c1db4c9ae02c12aeaeeff77ad9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff4064725854ba1826627b7a78a6a0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "52c00a1502fc2fe4e0ab",
+    "url": "/monsters-rolodex/static/css/main.b635b742.chunk.css"
+  },
+  {
+    "revision": "b42ad286debf87e51faf",
+    "url": "/monsters-rolodex/static/js/2.14105919.chunk.js"
+  },
+  {
+    "revision": "52c00a1502fc2fe4e0ab",
+    "url": "/monsters-rolodex/static/js/main.61f4d939.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
